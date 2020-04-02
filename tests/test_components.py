a25ca89b8870022f3100083b1ec5ac5ed9fbfa24
paraz <mail2paras.s@gmail.com>
2020-04-02 17:02:27 +0530

    add tests for checking hash consistency for ordered files and datetime info

--- tests/test_components.py
+++ tests/test_components.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from lambda_packaging.components import LambdaPackage
 import pulumi
 import shutil
@@ -16,9 +16,9 @@ class ResourceMock(pulumi.runtime.Mocks):
 
 pulumi.runtime.set_mocks(ResourceMock())
 
-test_hash_1 = "3fboMxcOb49OZuu95c5i6yfI6VZ8bo7RbLeKTOlRHpo="
-test_hash_2 = "hEnQV9VhzoU0v2r9EEfCQPx3htO3BYcWEdiQOzwp7Os="
-test_hash_3 = "5yDFAKYBmDzD/xwf+qHihYSUhwaAanqDzp5ptBc1NNk="
+test_hash_1 = "pkTEi+w+/P3ThNx31BdzMK1GVfBXvfdwTF8+uUkDlUQ="
+test_hash_2 = "fgPhqs9YpRQvT1dXXiy4w+T0AFSGkjNpdFVeLtggujQ="
+test_hash_3 = "Qn6WRrkmRHVgcOqY1QREst0Q4Y9mIuoXd9op3OMQI6w="
 
 
 class TestComponents(TestCase):
@@ -65,5 +65,41 @@ class TestComponents(TestCase):
 
             self.assertEqual(lambda_package.layer_hash, test_hash_3)
 
+    @pulumi.runtime.test
+    def test_archive_hash_consistency_when_files_not_ordered(self):
+        with patch("lambda_packaging.components.os") as mock_os:
+            # mock "sorted" function to diable sort behaviour
+            with patch("lambda_packaging.zip_package.sorted") as mock_sorted:
+                mock_sorted.side_effect = lambda x: x
+                mock_os.path.dirname.return_value = Path("tests/data")
+                lambda_package = LambdaPackage(
+                    name="example-test",
+                    layer=False,
+                    exclude=["**/requirements*.txt"],
+                    requirements_path="requirements_test_1.txt",
+                )
+
+                # asert archive hash not equal to detenmined hash
+                self.assertNotEqual(lambda_package.package_hash, test_hash_2)
+
+    @pulumi.runtime.test
+    def test_archive_hash_consistency_when_datetime_is_changed(self):
+        with patch("lambda_packaging.components.os") as mock_os:
+            # change datetime value of files being archived
+            with patch(
+                "lambda_packaging.zip_package.CONST_DATETIME", (2020, 2, 1, 0, 0, 0)
+            ):
+                mock_os.path.dirname.return_value = Path("tests/data")
+                lambda_package = LambdaPackage(
+                    name="example-test",
+                    layer=False,
+                    exclude=["**/requirements*.txt"],
+                    requirements_path="requirements_test_1.txt",
+                )
+
+                # asert archive hash not equal to detenmined hash
+                self.assertNotEqual(lambda_package.package_hash, test_hash_2)
+
     def tearDown(self):
+        # deletes the created assets after each test is run
         shutil.rmtree("tests/data/dist/", ignore_errors=True)
